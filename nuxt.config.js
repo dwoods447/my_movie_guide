9973860ee84478e551e9a3a52e541b19b3f2b26e
Demaria Woods <dpwoods@uh.edu>
2020-01-10 01:48:49 -0600

    added port nuxt config proces.env.port

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,8 @@ module.exports = {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: "https://nuxt-movie-guide-app.herokuapp.com"+':'+ process.env.port
+    baseURL: `https://nuxt-movie-guide-app.herokuapp.com`,
+    API_PORT: process.env.port
   },
   /*
   ** Build configuration
