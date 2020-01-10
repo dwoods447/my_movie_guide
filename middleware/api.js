abe60440e8d65591d619382a3d4db35f5fa463c3
Demaria Woods <dpwoods@uh.edu>
2020-01-10 00:21:13 -0600

    added base URL

--- middleware/api.js
+++ middleware/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: "https://nuxt-movie-guide-app.herokuapp.com/",
+  baseURL: "https://nuxt-movie-guide-app.herokuapp.com",
 
 });
 
